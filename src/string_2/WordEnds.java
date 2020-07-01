3ac33d7777196213c2117c7b0016ea05d9a158cb
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-07-01 21:45:41 +0200

    added solution for WordEnds

--- src/string_2/WordEnds.java
+++ src/string_2/WordEnds.java
@@ -9,23 +9,45 @@ wordEnds("XY1XY", "XY") → "11"
  */
 
 public class WordEnds {
-	public String wordEnds(String str, String word) {
-		if(str.length() <= 2) {
+	public static String wordEnds(String str, String word) {
+		if (str.length() <= 2) {
 			return "";
 		}
-		
-		int positionInStr = 0;
-		int wordPosition = str.indexOf(word, positionInStr);
-		if(wordPosition == - 1) {
-			return "";
-		}
-		
+
 		int wordLenght = word.length();
-		StringBuilder result = new StringBuilder();
+		StringBuilder result = new StringBuilder("");
+		int positionInStr = 0;
+		int wordPosition;
 		
-		for(int i = 0; i < str.length() - wordLenght; ) {
+		for (; positionInStr < str.length() - wordLenght;) {
 			
+			if(wordLenght == 1) {
+				wordPosition = str.indexOf(word, positionInStr + 1);
+			} else {
+				wordPosition = str.indexOf(word, positionInStr);
+			}
+			
+			if (wordPosition == -1) {
+				return result.toString();
+			}			
+			
+			if (wordPosition == 0) {
+				result.append(str.charAt(wordPosition + wordLenght));
+				positionInStr = (wordPosition + (wordLenght - 1));
+			} else if (wordPosition == str.length() - wordLenght) {
+				result.append(str.charAt(str.length() - (wordLenght + 1)));
+				positionInStr = (wordPosition + (wordLenght - 1));
+			} else {
+				result.append(str.charAt(wordPosition - 1));
+				result.append(str.charAt(wordPosition + wordLenght));
+				positionInStr = (wordPosition + (wordLenght - 1));
+			}
 		}
 		
+		return result.toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(wordEnds("XYXY", "XY"));
 	}
 }
