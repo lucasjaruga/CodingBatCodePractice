61f8fd8895d1b83df3a996d2a9b4cbafbca50e25
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-08-02 04:33:57 +0200

    added solution for EqualIsNot

--- src/string_3/EqualIsNot.java
+++ src/string_3/EqualIsNot.java
@@ -10,10 +10,32 @@ equalIsNot("noisxxnotyynotxisi") → true
 
 class EqualIsNot {
 	public static boolean equalIsNot(String str) {
-	
-		
+
+		int isCounter = 0;
+		int notCounter = 0;
+
+		// counting "is"
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (str.substring(i, i + 2).equals("is")) {
+				isCounter++;
+			}
+		}
+
+		// counting "not"
+		for (int i = 0; i < str.length() - 2; i++) {
+			if (str.substring(i, i + 3).equals("not")) {
+				notCounter++;
+			}
+		}
+
+		// return result
+		if (isCounter == notCounter) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println(equalIsNot("This is not"));
 	}
